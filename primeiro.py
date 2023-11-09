f394f6f976f42c89a70538aebf283c8775605fdb
devweslly <devweslly@gmail.com>
2023-11-09 14:19:43 -0300

    Primeiros comandos

---
+++ primeiro.py
@@ -0,0 +1,13 @@
+# Imprime um valor na tela (Saída de Dados)
+print("Hello, Word!")
+
+# Entrada de Dados
+nome: str = input('Qual o seu nome?')
+
+print("Prazer em te conhecer, {}".format(nome))
+
+idade: int = input('Qual a sua idade?')
+
+# Informando o tipo das variáveis
+print("A variável nome é do tipo: {b1}".format(b1 = type(nome)))
+print("A variável idade é do tipo: {b1}".format(b1 = type(idade)))
