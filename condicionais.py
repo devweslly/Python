17527e24a66c485228551eeca5049c94cfe25103
devweslly <devweslly@gmail.com>
2023-11-11 18:13:53 -0300

    Trabalhando com estrutura condicional

---
+++ condicionais.py
@@ -0,0 +1,18 @@
+idade = 20
+
+if idade >= 18:
+    print('Você é maior de idade.')
+else:
+    print( 'Você é menor de idade.')
+
+media = 8.0
+presenca = 70
+
+if media == 10:
+    print('Aprovado')
+elif media >= 7 and presenca >= 70:
+    print('Aprovado')
+elif media >= 5 or presenca >= 70:
+    print('Vai fazer a sub')
+else:
+    print('Reprovado')
