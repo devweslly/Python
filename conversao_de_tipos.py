d8b9e7c18d36bbd20fdd18b9241dc031ff1c885d
devweslly <devweslly@gmail.com>
2023-11-11 16:51:46 -0300

    Trabalhando com conversões de tipo

---
+++ conversao_de_tipos.py
@@ -0,0 +1,16 @@
+# Conversão de tipos
+
+idade = '32'
+num1 = '10'
+num2 = '20'
+
+print(idade, type(idade))
+
+# conversão explicita (conversão manual)
+idade_inteira = int(idade)
+
+print(idade_inteira, type(idade_inteira))
+
+altura = float( input('Informe sua altura em metros: ') )
+
+print(altura, type(altura))
