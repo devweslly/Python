c4775524731bbc96b9021718944ce921c612f076
devweslly <devweslly@gmail.com>
2023-11-11 20:21:41 -0300

    Conhecendo métodos úteis para listas

--- listas.py
+++ listas.py
@@ -35,3 +35,38 @@ print(lista[0:3]) # obtendo os elementos do intervalo de indices 0 até 2
 print(lista[3:7])
 print(lista[3:])
 print(lista[0:10:2])
+
+# adicionando um elemento no final da lista
+lista.append(160)
+
+# escolhendo a posição onde o elemento inserido (e empurra todos os elementos para frente)
+lista.insert(3 , 200)
+
+# juntando duas listas (colocando os elementos da segunda lista no final da primeira)
+lista.extend(notas)
+
+# removendo elemento (caso não especificar a posição é removido o último elemento)
+lista.pop() # remove ultimo elemento
+lista.pop(0) # remove elemento do indice especificado
+
+# o remove você informa qual valor remover
+lista.remove(200)
+
+# Contando a quantidade de vezes que um valor aparece na lista
+print('Quantidade de 2 na lista: ', lista.count(2))
+
+# Informando o indice de um valor
+print('Indice do valor 77: ', lista.index(77))
+
+print('Lista depois da alterações: ', lista)
+
+# Ordenando a lista
+lista.sort()
+print(lista)
+
+# Somando os valores da lista
+print(sum(lista))
+
+# O valor max e min
+print('Maior valor da lista', max(lista))
+print('Menor valor da lista', min(lista))
